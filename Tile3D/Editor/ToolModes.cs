d533f8281af52373ac3f9a98cd806c2d4e4da13f
Vinicius <Vinicius@VAPOR>
2020-09-10 00:10:38 -0300

    Multiple Selections

---
+++ Tile3D/Editor/ToolModes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ToolModes
+{
+    Transform,
+    Building,
+    Painting
+}
+
+public enum PaintModes
+{
+    Brush,
+    Fill
+}
